47de0aa3990a130571221ccd5c288fc0aba53d6b
Jaga Deep <jagasiva1999@gmail.com>
2023-02-14 20:18:04 +0530

    Environment variable added for API Key

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 export const geoApioptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '534a63a8c7msh37d49b1ac482585p1242f7jsn54c8e43c9513',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+		'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}` ,
+		'X-RapidAPI-Host': "wft-geo-db.p.rapidapi.com"
 	}
 }; 
 
