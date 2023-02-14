ce7987755f29ed19fcacd4f65297ab42b69d7d96
Jaga Deep <jagasiva1999@gmail.com>
2023-02-14 18:21:50 +0530

    Project in Process

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApioptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '534a63a8c7msh37d49b1ac482585p1242f7jsn54c8e43c9513',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+}; 
+
+export const GEO_API_URL ='https://wft-geo-db.p.rapidapi.com/v1/geo'
+
