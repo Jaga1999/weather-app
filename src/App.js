b7cb66f8684b294ac654ccda1d671046b6bdf07d
Jaga1999 <jagasiva1999@gmail.com>
2023-02-15 19:55:38 +0530

    Project Finished

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React from 'react'
-import Search from './components/search/Search'
-import CurrentWeather from './components/current-Weather/CurrentWeather'
+import React, { useState } from "react";
+import Search from "./components/search/Search";
+import CurrentWeatherComp from "./components/current-Weather/CurrentWeatherComp";
+import "./App.css";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api/api";
+import Forecast from "./components/forecast/Forecast";
 
 const App = () => {
+  const [CurrentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setforecast] = useState(null);
 
-  const handleonSearchChange =(searchData) =>{
-    console.log(searchData)
-  }
+  const handleonSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
 
+    const CurrentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([CurrentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setforecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
   return (
-    <div className='container'>
-      
-      <Search onSearchChange={handleonSearchChange}/>
-      <CurrentWeather />
+    <div className="container">
+      <Search onSearchChange={handleonSearchChange} />
+      {CurrentWeather && <CurrentWeatherComp data={CurrentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,44 +1,43 @@
-import React, { useState } from 'react'
-import { AsyncPaginate } from 'react-select-async-paginate'
-import { GEO_API_URL, geoApioptions } from '../../api'
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApioptions } from "../../api/api";
 
-const Search = ({onSearchChange}) => {
-    const [Search, setSearch] = useState(null)
+const Search = ({ onSearchChange }) => {
+  const [Search, setSearch] = useState(null);
 
-    const loadOptions = (inputValue) => {
-        
-        return fetch(
-            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-         geoApioptions
-         )
-        .then((response) => response.json())
-        .then((response) => {
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApioptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
             return {
-                options: response.data.map((city) =>{
-                    return{
-                        value: `${city.latitude} ${city.longitude}`,
-                        label: `${city.name}, ${city.countryCode}`,
-                    }
-                })
-            }
-        })
-        .catch((err) => console.error(err));
-    }
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
 
-    const handleonChange = (searchData) =>{
-        setSearch(searchData);
-        onSearchChange(searchData);
-    }
+  const handleonChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
 
   return (
     <AsyncPaginate
-        placeholder="Search for city"
-        debounceTimeout={600}
-        value={Search}
-        onChange={handleonChange}
-        loadOptions={loadOptions}
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={Search}
+      onChange={handleonChange}
+      loadOptions={loadOptions}
     />
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
